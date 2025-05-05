adabe8b6add59d0f1f14926b68d96149b421325e
sayedhamed3-ga <sayedhamed2004@gmail.com>
2025-05-05 14:14:27 +0300

    Added Worker model

--- models/Worker.js
+++ models/Worker.js
@@ -1,7 +1,22 @@
 const {Schema, model} = require("mongoose")
 
 const workerSchema = new Schema({
-
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        text: true
+    },
+    currentLocation: {
+        lat: Number,
+        lng: Number
+    },
 })
 
 const Worker = model("Worker", workerSchema)
