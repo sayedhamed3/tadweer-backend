3cf7de64ea1e972a9330cc366f22887fbd89ee4b
sayedhamed3-ga <sayedhamed2004@gmail.com>
2025-05-05 18:16:08 +0300

    Added Achievement Model

--- models/Achievement.js
+++ models/Achievement.js
@@ -1,7 +1,30 @@
 const {Schema, model} = require("mongoose")
 
 const achievementSchema = new Schema({
-
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        text: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    badgeIcon: {
+        type: String,
+        required: true
+    },
+    material: {
+        type: Schema.Types.ObjectId,
+        ref: "Material",
+        required: true
+    },
+    thresholdQuantity: {
+        type: Number,
+        required: true
+    },
 })
 
 const Achievement = model("Achievement",achievementSchema)
